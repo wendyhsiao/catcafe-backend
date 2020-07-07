254e19f09db2ed54587a93616e2bff884084dbd5
wendy.hsiao <wendy19940331@gmail.com>
2020-07-07 10:44:39 +0800

    【加入】建立 cafe seeder

---
+++ seeders/20200706091547-cafe-seed.js
@@ -0,0 +1,28 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Cafes',
+      Array.from({ length: 20 }).map(cafe => ({
+        name: faker.name.findName(),
+        tel: faker.phone.phoneNumberFormat(),
+        address_city: faker.address.county(),
+        address_dist: faker.address.city(),
+        address_other: faker.address.streetAddress(),
+        opening_hour: '8:00 - 6:00',
+        consumption_patterns: faker.lorem.sentences(),
+        rule: faker.lorem.sentences(),
+        other: faker.lorem.text(),
+        minimum_charge: '150',
+        facebook: faker.lorem.word(),
+        instagram: faker.lorem.word(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Cafes', null, {})
+  }
+}
