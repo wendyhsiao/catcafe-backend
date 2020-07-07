568b76d62752dbf2bec8b2625508f716f4d1746b
wendy.hsiao <wendy19940331@gmail.com>
2020-07-07 13:13:56 +0800

    【加入】瀏覽基本 cafe 首頁

--- controllers/cafeController.js
+++ controllers/cafeController.js
@@ -1,8 +1,40 @@
+const db = require('../models')
+const { Op } = require('sequelize')
+const Cafe = db.Cafe
+
 const cafeController = {
-  getCafes: (req, res) => {
-    const cafes = 'cat'
-    console.log(cafes)
-    return res.json({ cafes })
+  getCafes: async (req, res) => {
+    try {
+      const searchQuery = '%' + req.query.q + '%'
+
+      console.log('searchQuery', searchQuery)
+      const cafes = await Cafe.findAll({
+        raw: true,
+        where: {
+          [Op.or]: [
+            {
+              address_city: {
+                [Op.like]: searchQuery
+              }
+            },
+            {
+              address_dist: {
+                [Op.like]: searchQuery
+              }
+            },
+            {
+              name: {
+                [Op.like]: searchQuery
+              }
+            }
+          ]
+        }
+      })
+      console.log(cafes)
+      return res.json({ cafes })
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
