c8175d62890ee1e34f80ab9f7f1c62b8414aca96
wendy.hsiao <wendy19940331@gmail.com>
2020-07-08 18:54:03 +0800

    【加入】註冊後台會員帳號

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
---
+++ controllers/admin/userController.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const { AdminUser } = db
+
+const userController = {
+  signUp: async (req, res) => {
+    try {
+      const { name, email, password, password2 } = req.body
+      // 確認所有項目都有填
+      if (!name || !email || !password || !password2) {
+        return res.json({ status: 'error', message: '所有都為必填項目！' })
+      }
+      // 確認密碼兩次輸入的一樣
+      if (password !== password2) {
+        return res.json({ status: 'error', message: '兩次密碼輸入不同！' })
+      }
+      // 確認 email 格式
+      const regexp = / ^ ([\w\.\-]){1,64} \@ ([\w\.\-]){1,64} $ /
+      if (!email.match(regexp)) {
+        return res.json({ status: 'error', message: '此 email 格式無效！' })
+      }
+      // 確認 email 是否註冊過
+      const isExist = await AdminUser.findOne({ where: { email } })
+      if (isExist) {
+        return res.json({ status: 'error', message: '此 email 已註冊過！' })
+      }
+
+      await AdminUser.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      })
+
+      return res.json({ status: 'success', message: '成功註冊帳號！' })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+module.exports = userController
