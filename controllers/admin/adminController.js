043766ced2ebe5cdac55a9094c059a351358c92e
wendy.hsiao <wendy19940331@gmail.com>
2020-07-09 19:18:38 +0800

    【加入】瀏覽後台 cafes 頁 api

---
+++ controllers/admin/adminController.js
@@ -0,0 +1,42 @@
+const db = require('../../models')
+const { Cafe } = db
+const { Op } = require('sequelize')
+
+const adminController = {
+  getCafes: async (req, res) => {
+    try {
+      const pageLimit = 15 // 每頁 15 筆
+      let offset = 0 // 偏移數
+      if (req.query.page) {
+        offset = (req.query.page - 1) * pageLimit
+      }
+
+      const searchQuery = req.query.q
+      let where = {}
+      if (searchQuery !== undefined) {
+        where = {
+          [Op.or]: [ // 搜尋時查找店名
+            {
+              name: {
+                [Op.like]: `%${searchQuery}%`
+              }
+            }
+          ]
+        }
+      }
+
+      const cafes = await Cafe.findAndCountAll({
+        order: [['id', 'DESC']], // 預設排序新至舊
+        limit: pageLimit,
+        offset,
+        where
+      })
+
+      return res.json({ cafes })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+module.exports = adminController
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const adminController = require('../controllers/admin/adminController.js')
 const userController = require('../controllers/admin/userController.js')
 
+router.get('/cafes', adminController.getCafes)
 
 router.post('/signup', userController.signUp)
 router.post('/signin', userController.signIn)
